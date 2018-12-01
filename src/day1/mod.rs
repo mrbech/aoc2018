ec1641c18c256853823ea97223bc4becf8792b80
Martin Bech <martin@bech.io>
2018-12-01 20:12:00 +0100

    part 2 solution

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,12 +1,37 @@
 pub mod part1 {
+    use super::*;
+
     pub fn run() {
         println!("Running Day 1, Part 1");
-
-        let input = include_str!("part1.txt");
-        let result: i32 = input.split_whitespace()
-            .map(|s| s.parse::<i32>().expect("failed to parse number"))
-            .sum();
-
+        let result: i32 = parse_input().into_iter().sum();
         println!("Result: {:?}", result);
     }
+}
+
+pub mod part2 {
+    use super::*;
+    use std::collections::HashSet;
+    pub fn run() {
+        println!("Running Day 1, Part 2");
+        let mut frequencies = parse_input().into_iter()
+            .cycle()
+            .scan(0, |f, c| {
+                *f = *f + c;
+                Some(*f)
+            });
+
+        let mut freq_seen = HashSet::new();
+        let result = frequencies.find(|c| !freq_seen.insert(*c));
+        match result {
+            Some(r) => println!("Result: {:?}", r),
+            None => println!("Result: Did not find a repeating frequency")
+        }
+    }
+}
+
+fn parse_input() -> Vec<i32> {
+    let input = include_str!("input.txt");
+    input.split_whitespace()
+        .map(|s| s.parse::<i32>().expect("failed to parse number"))
+        .collect()
 }
